2d60da680769e88dd6f467354396209e23ddba0b
Mario Pastuovic <mario.pastuovic@burza.hr>
2017-12-20 14:35:51 +0100

    array and json helper methods

--- source/processor/core/Ralph.php
+++ source/processor/core/Ralph.php
@@ -86,4 +86,71 @@ class Ralph
         }
     }
 
+    // returns array from converted multidimensional object
+    public static function objToArray($obj)
+    {
+        // Not an object or array
+        if (!is_object($obj) && !is_array($obj)) {
+            return $obj;
+        }
+
+        // Parse array
+        foreach ($obj as $key => $value) {
+            $arr[$key] = Ralph::objToArray($value);
+        }
+
+        // Return parsed array
+        return $arr;
+    }
+
+    // returns difference between two multidimensional arrays as an array
+    public static function compareArrays($array1, $array2){
+        $result = array();
+
+        foreach($array1 as $key => $val) {
+            if(is_array($val) && isset($array2[$key])) {
+                $tmp = Ralph::compareArrays($val, $array2[$key]);
+                if($tmp) {
+                    $result[$key] = $tmp;
+                }
+            }
+            elseif(!isset($array2[$key])) {
+                $result[$key] = null;
+            }
+            elseif($val !== $array2[$key]) {
+                $result[$key] = $array2[$key];
+            }
+
+            if(isset($array2[$key])) {
+                unset($array2[$key]);
+            }
+        }
+
+        $result = array_merge($result, $array2);
+
+        return $result;
+    }
+
+    // returns difference between two json strings as json string
+    public static function compareJson($json1, $json2){
+        $data1 = json_decode($json1, true);
+        $data2 = json_decode($json2, true);
+
+        $data1 = Ralph::objToArray($data1);
+        $data2 = Ralph::objToArray($data2);
+
+        $data = Ralph::compareArrays($data1, $data2);
+        $array = Ralph::cleanEmptyArrayElemets($data);
+
+        return json_encode($array);
+    }
+
+    // removes empty elements from an array
+    public static function cleanEmptyArrayElemets($data){
+
+        $array = array_map('array_filter', $data);
+        $array = array_filter($array);
+
+        return json_encode($array);
+    }
 }
